10a067df2039840d69551fefe5fae0a54ffd3782
Zayne <star1582@naver.com>
2024-03-28 12:31:00 +0000

    Google Api Key Issue #1

--- functions.php
+++ functions.php
@@ -19,25 +19,25 @@ function pageBanner($args = NULL) {
   }
 
   ?>
-  <div class="page-banner">
+<div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
-      <div class="page-banner__intro">
-        <p><?php echo $args['subtitle']; ?></p>
-      </div>
-    </div>  
-  </div>
+        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+        <div class="page-banner__intro">
+            <p><?php echo $args['subtitle']; ?></p>
+        </div>
+    </div>
+</div>
 <?php }
 
-function university_files() {
-  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDin3iGCdZ7RPomFLyb2yqFERhs55dmfTI', NULL, '1.0', true);
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
-}
+// function university_files() {
+//   wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDin3iGCdZ7RPomFLyb2yqFERhs55dmfTI', NULL, '1.0', true);
+//   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+//   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+//   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+//   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+//   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+// }
 
 add_action('wp_enqueue_scripts', 'university_files');
 
